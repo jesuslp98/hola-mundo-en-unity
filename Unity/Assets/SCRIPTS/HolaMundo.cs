c048317b30b541ee2abd64472fce0e672f1c84e1
jesuslp98 <franciscojesuslaguna@ceitartessos.com>
2019-10-21 16:42:43 +0200

    primera subida a Github

---
+++ Unity/Assets/SCRIPTS/HolaMundo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hola Mundo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
